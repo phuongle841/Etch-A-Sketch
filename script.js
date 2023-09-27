afaa7b18e7474d01b04b35925b00e44f037ec91a
phuong le <phuongleminh841@gmail.com>
2023-09-27 16:39:48 +0700

    refactor board rewrite function and add more logic structure

--- script.js
+++ script.js
@@ -1,31 +1,81 @@
 const body = document.querySelector("body");
 const board = document.querySelector(".board");
-console.log(board);
-const bigDivArray = [];
+
+let numberOfBox ;
 const smallDivArray = [];
-numberOfBox = 15;
-for (let i = 0; i < numberOfBox; i++) {
-  let bigDiv = document.createElement("div");
-  bigDiv.classList.add("bigDiv");
-  bigDivArray.push(bigDiv);
+const bigDivArray = [];
+let currentWidth = board.offsetWidth;
+let currentHeight = board.offsetHeight;
+
+function getLineHeight() {
+  let currentLineHeight = Math.floor(currentWidth / numberOfBox);
+  return currentLineHeight;
 }
-bigDivArray.forEach((bigDiv) => {
-  board.appendChild(bigDiv);
-});
-bigDivArray.forEach((bigDiv) => {
-  for (let j = 0; j < numberOfBox; j++) {
-    let div = document.createElement("div");
-    div.classList.add("smallDiv");
-    bigDiv.appendChild(div);
-    smallDivArray.push(div);
+
+function SetNumberOfBox(boxes) {
+  numberOfBox = boxes;
+}
+
+function emptyArray(Array) {
+  while (Array.length > 0) {
+    Array.pop();
   }
-});
-function paintSmallDiv() {
+}
+
+function setLine(numberOfBox) {
+  emptyArray(bigDivArray);
+  let currentLineHeight = getLineHeight();
+  for (let i = 0; i < numberOfBox; i++) {
+    let bigDiv = document.createElement("div");
+    bigDiv.setAttribute("style", `height: ${currentLineHeight}px;`);
+    bigDiv.classList.add("bigDiv");
+    bigDivArray.push(bigDiv);
+  }
+  bigDivArray.forEach((bigDiv) => {
+    board.appendChild(bigDiv);
+  });
+}
+
+function setRow(numberOfBox) {
+  emptyArray(smallDivArray);
+  let currentLineHeight = getLineHeight();
+  bigDivArray.forEach((bigDiv) => {
+    for (let j = 0; j < numberOfBox; j++) {
+      let div = document.createElement("div");
+      div.setAttribute(
+        "style",
+        `width:${currentLineHeight}px;height: ${currentLineHeight}px;`
+      );
+      div.classList.add("smallDiv");
+      bigDiv.appendChild(div);
+      smallDivArray.push(div);
+    }
+  });
+}
+
+function eraseBoard() {
+  board.innerHTML = "";
+}
+
+function changeColor(e) {
+  if (e.type === "mouseover" && !mouseDown) return;
   e.target.classList.add("hover");
 }
 
-smallDivArray.forEach((smallDiv) => {
-  smallDiv.addEventListener("mouseover", (event) => {
-    smallDiv.classList.add("hover");
+function addPaintEffect() {
+  smallDivArray.forEach((smallDiv) => {
+    smallDiv.addEventListener("mouseover", changeColor);
   });
-});
+}
+
+function ReWorkBoard(numberOfBox) {
+  SetNumberOfBox(numberOfBox);
+  setLine(numberOfBox);
+  setRow(numberOfBox);
+  addPaintEffect();
+}
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+ReWorkBoard(4);
